61a2b9dd9dffdc24346ae611e16227f848f9c72f
NguyenGiaBao23520121 <nguyengiabao.bh2005@gmail.com>
2026-03-18 18:51:22 +0700

    Add Spring Boot Java backend project

---
+++ back-end/src/test/kotlin/com/example/bookinglite/BookingliteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.bookinglite
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookingliteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
