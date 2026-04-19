f2797447eb3006fca4679b8e90b56ac6831d71bc
NguyenGiaBao23520121 <nguyengiabao.bh2005@gmail.com>
2026-04-19 18:30:23 +0700

    Nothing

--- android/app/src/main/java/com/example/travelapp/utils/Secrets.kt
+++
@@ -1,7 +0,0 @@
-package com.example.travelapp.utils
-
-object Secrets {
-    const val CLOUDINARY_CLOUD_NAME = "ten_cloud_cua_ban"
-    const val CLOUDINARY_API_KEY = "123456789"
-    const val CLOUDINARY_API_SECRET = "abcxyz_secret_cua_ban"
-}
